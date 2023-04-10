4c0699913f813c53b17acb6c3865f7a2d4dbb0d6
mananjain31 <mananjain31jan@gmail.com>
2023-04-11 02:07:38 +0530

    :hammer: dev script added and its scripts command configured

---
+++ script.ts
@@ -0,0 +1,19 @@
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
+
+const main = async () => {
+  const user = await prisma.user.create({
+    data: {
+      name: "Manan",
+    },
+  });
+  console.log(user);
+};
+
+main()
+  .catch((e) => {
+    console.log(e.message);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
